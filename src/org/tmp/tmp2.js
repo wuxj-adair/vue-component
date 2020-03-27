4cab88d26ba3a017db3cc1c4a7cef8dd76e89dff
wuxiangjun <wuxiangjun@shoubakeji.com>
2020-03-27 09:23:08 +0800

    no

--- src/org/tmp/tmp2.js
+++ src/org/tmp/tmp2.js
@@ -1,3 +1,11 @@
+/*
+ * @Author: your name
+ * @Date: 2020-03-24 15:30:04
+ * @LastEditTime: 2020-03-24 17:10:14
+ * @LastEditors: Please set LastEditors
+ * @Description: In User Settings Edit
+ * @FilePath: \vue-component-book-master\src\org\tmp\tmp2.js
+ */
 var obj = {0: 'a', 1: 'b', 2:'c', length: 3};
 Array.from(obj, function(value, index){
   console.log(value, index, this, arguments.length);
