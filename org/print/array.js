0cd24402128131abd49fb91aed167ad941d14530
wuxiangjun <wuxiangjun@shoubakeji.com>
2020-05-18 15:04:37 +0800

    update

--- org/print/array.js
+++ org/print/array.js
@@ -1,7 +1,7 @@
 //数组去重 利用对象
 let arr = [1, 2, 2, 3];
-let newArr = Array.from(new Set(arr));
-function distinct(a, b) {
+
+function distinct(a, b=[]) {
     let arr = a.concat(b)
     let result = []
     let obj = {}
@@ -15,6 +15,10 @@ function distinct(a, b) {
 
     return result
 }
+let newArr = distinct(arr)
+let newArr2 = Array.from(new Set(arr));
+let newArr3 = [...new Set(arr)];
+console.log(arr,newArr, newArr2, newArr3)
 
 //类数组转数组
 Array.prototype.slice.call(arrayLike);
--- org/print/prototype.js
+++ org/print/prototype.js
@@ -7,3 +7,14 @@ function inherit(child, parent) {
     // 重写被污染的子类的constructor
     child.prototype.constructor = child
 }
+
+//instanceof      a instanceof A
+function instanceOf(left,right) {
+    let proto = left.__proto__;
+    let prototype = right.prototype
+    while(true) {
+        if(proto === null) return false
+        if(proto === prototype) return true
+        proto = proto.__proto__;
+    }
+}
