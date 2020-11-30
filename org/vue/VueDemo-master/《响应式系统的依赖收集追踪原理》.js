1236b4004224bc3177246bc7eec688b33affd4d7
wxj <wuxj_adair@163.com>
2020-11-30 16:25:04 +0800

    update

--- org/vue/VueDemo-master/《响应式系统的依赖收集追踪原理》.js
+++ org/vue/VueDemo-master/《响应式系统的依赖收集追踪原理》.js
@@ -16,6 +16,7 @@ class Dep {
 
 class Watcher {
     constructor () {
+        //dep.target = this是把dep.target指向new出来的Watcher实例本身。
         Dep.target = this;
     }
 
@@ -35,12 +36,20 @@ function observer (value) {
 }
 
 function defineReactive (obj, key, val) {
+    //首先我们来实现一个订阅者 Dep ，它的主要作用是用来存放 Watcher 观察者对象。
     const dep = new Dep();
     
     Object.defineProperty(obj, key, {
         enumerable: true,
         configurable: true,
         get: function reactiveGetter () {
+            //Dep.target是引用的，目的是将当前这个Watcher收集到所有依赖的Dep中去。例子中只有一个，是为了便于理解，实际上可能会不止一个Dep去收集同一个Watcher的。
+            //dep.addSub(Dep.target) 为什么不用 dep.addSub(new Watcher())
+            //因为实际上一个Watcher对象可能会在多个Sub中的哟～
+            //在这里的watcher对象是对应视图的，同样的watcher对象在实际上被push进去的时候会检查是否已经存在，不会多个重复的放在同一个dep中的。然后同一个属性是可能对应多个视图的更新的，最常见就是在使用vuex的时候全局store对应在多个视图中体现。
+            //实际上是有去重的逻辑的，这些逻辑对于理解原理是多余的所以我省略了。回答一下你的疑问：Watcher是有一个id属性的，每个Watcher的id是不一样的，用这个id就可以去重，在被push进去之前会先看看是否已经有相同id的Watcher存在即可～
+           //data是Obj的实际上是递归调用执行的，Dep.target你可以想象一下一个Watcher对象在多个Dep中的场景，可以参考vuex场景。
+            //一个对象属性对应一个dep，一个dep对应多个watcher(一个对象属性可能再多个标签使用，那么就会有对应多个watcher，这些watcher都会放入到这个对象属性唯一对应的dep中)，这是Vue1.0的实现，但数据过大时，就会有很多个watcher，就会出现性能问题；所以在Vue2.0中引入的VDOM，给每个vue组件绑定一个watcher，这个组件上的数据的dep中都包含有该watcher，当该组件数据发生变化时，就会通知watcher触发update方法，生成VDOM，和旧的VDOM进行比较，更新改变的部分，极大的减少了watcher的数量，优化了性能；（所以，在Vue2.0中是一个组件对应一个watcher）
             dep.addSub(Dep.target);
             return val;         
         },
