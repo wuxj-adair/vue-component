26e2d70ba79cccc14c4c7c9dcd0dcb6b4e1dc66b
wxj <wuxj_adair@163.com>
2020-12-02 09:56:55 +0800

    1

--- org/print/apply.js
+++ org/print/apply.js
@@ -14,7 +14,7 @@ Function.prototype.myApply = function(context=window, args=[]) {
 //bind
 //就是返回一个函数,里面执行了apply上述的操作而已.
 //不过有一个需要判断的点, 因为返回新的函数, 要考虑到使用new去调用, 并且new的优先级比较高, 所以需要判断new的调用, 
-//还有一个特点就是bind调用的时候可以传参, 调用之后生成的新的函数也可以传参,
+//还有一个特点就是bind调用的时候可以传参, 调用之后生成的新的函数也可以传参,q
 Function.prototype.myBind = function (context, ...args) {
     const fn = this
     args = args ? args : []
